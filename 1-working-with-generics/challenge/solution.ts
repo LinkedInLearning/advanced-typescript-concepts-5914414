915edf21538d7f76d438bc7b26fd1053e74bc82b
BrightBoost <info@brightboost.nl>
2024-06-05 22:29:53 +0200

    starter

---
+++ 1-working-with-generics/challenge/solution.ts
@@ -0,0 +1,12 @@
+// Create a type-safe, generic sorting function for an array of objects 
+// based on a property key of the object provided at runtime.
+ 
+// The function must maintain type safety for property keys.
+
+// Complete this function
+function sortArray() {}
+
+// Test your sorting function with the following array
+let tasks = [{ id: 2, title: "Write Code" }, { id: 1, title: "Compile" }];
+// let sortedTasks = sortArray(tasks, "id");
+// console.log(sortedTasks);  
