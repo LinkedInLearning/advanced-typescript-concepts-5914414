20131a8c83a08df26887ae6fea53705e509117b5
BrightBoost <info@brightboost.nl>
2024-06-08 00:29:00 +0200

    start

---
+++ 6-decorators-and-metadata-reflection/6-1-understanding-decorators/logClassExample.ts
@@ -0,0 +1,8 @@
+function logClass(target: Function) {
+  console.log(`Class in use: ${target.name}`);
+}
+
+//@logClass
+class User {
+  constructor(public name: string, public age: number) { }
+}
---
+++ 6-decorators-and-metadata-reflection/6-1-understanding-decorators/logMethodExample.ts
@@ -0,0 +1,21 @@
+function logMethod(target: any, key: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  descriptor.value = function (...args: any[]) {
+    const start = performance.now();
+    const result = originalMethod.apply(this, args);
+    const finish = performance.now();
+    console.log(`${key} executed in ${finish - start} milliseconds`);
+    return result;
+  };
+  return descriptor;
+}
+
+class MathOperations {
+  @logMethod
+  add(x: number, y: number): number {
+    return x + y;
+  }
+}
+
+const math = new MathOperations();
+math.add(5, 3);  // Output: "add executed in X milliseconds"
