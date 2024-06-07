7ca9df204103fa396bc811f08b815ca541d2b202
BrightBoost <info@brightboost.nl>
2024-06-08 00:33:53 +0200

    starter code

---
+++ 6-decorators-and-metadata-reflection/challenge/solution.ts
@@ -0,0 +1,9 @@
+class ProductManagement {
+  createProduct(name: string, price: number) {
+    console.log(`Product created: ${name}, Price: ${price}`);
+  }
+
+  deleteProduct(name: string) {
+    console.log(`Product deleted: ${name}`);
+  }
+}
