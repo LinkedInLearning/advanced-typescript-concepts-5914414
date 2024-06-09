b930af7233825dcaaad53dc9ea38659f348eb4b0
BrightBoost <info@brightboost.nl>
2024-06-10 00:23:58 +0200

    solution

--- 6-decorators-and-metadata-reflection/challenge/productManagement.ts
+++ 6-decorators-and-metadata-reflection/challenge/productManagement.ts
@@ -1,9 +1,17 @@
-export class ProductManagement {
+import { Log, Authorize, Role } from './decorators';
+
+class ProductManagement {
+  @Log
+  @Role("admin")
+  @Authorize("admin")
   createProduct(name: string, price: number) {
     console.log(`Product created: ${name}, Price: ${price}`);
   }
 
+  @Log
   deleteProduct(name: string) {
     console.log(`Product deleted: ${name}`);
   }
 }
+
+export default ProductManagement;
