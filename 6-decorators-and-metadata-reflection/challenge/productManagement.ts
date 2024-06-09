e77ad98c9be70f09070eb20e26fc47754717862a
BrightBoost <info@brightboost.nl>
2024-06-09 22:30:34 +0000

    solution

--- 6-decorators-and-metadata-reflection/challenge/productManagement.ts
+++ 6-decorators-and-metadata-reflection/challenge/productManagement.ts
@@ -2,7 +2,7 @@ import { Log, Authorize, Role } from './decorators';
 
 class ProductManagement {
   @Log
-  @Role("admin")
+  @Role("user")
   @Authorize("admin")
   createProduct(name: string, price: number) {
     console.log(`Product created: ${name}, Price: ${price}`);
